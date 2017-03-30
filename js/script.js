ec31646176e159dfc4b6c5e97272d6d7976e4479
Tim Hall <timhall@apple.com>
2017-03-30 22:24:51 +0800

    Getting empty response from nyt

--- js/script.js
+++ js/script.js
@@ -21,9 +21,20 @@ function loadData() {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
-    // YOUR CODE GOES HERE!
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=af809ecfef4b48abb7355058ce86e862'
+    $.getJSON(nytimesUrl, function(data){
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+        };
+    });
 
     return false;
 };
+
+
 
 $('#form-container').submit(loadData);
